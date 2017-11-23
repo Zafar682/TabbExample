ed199636ebfea32c16f67d401dcbe4507c1063fc
Zafar682 <zafar.0822@mail.ru>
2017-11-23 19:43:50 +0500

    tabli quran

---
+++ app/src/main/java/com/example/administrator/tabbexample/Tab1Quran.java
@@ -0,0 +1,23 @@
+package com.example.administrator.tabbexample;
+
+/**
+ * Created by administrator on 11/22/17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Quran extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1quran, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/tabbexample/Tab3Bookmarks.java
@@ -0,0 +1,22 @@
+package com.example.administrator.tabbexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by administrator on 11/22/17.
+ */
+
+public class Tab3Bookmarks extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3bookmarks, container, false);
+        return rootView;
+    }
+
+}
